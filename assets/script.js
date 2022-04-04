7253d9c76378e2576ad06ea4c3853e795dc0fa6d
mateonav98 <mateonav98@gmail.com>
2022-04-04 16:13:28 -0700

    adding initial basic html quiz layout

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+// Specifications: 
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// need: 
+// start button 
+// add event listener
+// 
+
+// what do we do?
+// 1. offer user with a question and multuple choice answers when they press start
+// 2. capture user input based their answers
+// 3. increment/decrement score with each user input
+// 4. decrease time any wrong answer selected
+// 5. Game over once all questions have been answered or time elapses
+// 6. save user score
+
